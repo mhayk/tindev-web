a3241ac49cfc721f6429193fa014c42340174819
Mhayk Whandson <hi@mhayk.com>
2019-08-08 16:04:46 +0100

    ESLint has been removed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import './App.css';
-
+import React from 'react'
+import './App.css'
 
 import Routes from './routes'
 
-function App() {
-  return (
-    <Routes />
-  );
-}
+const App = () => <Routes />
 
-export default App;
+export default App
