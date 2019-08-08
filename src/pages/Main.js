c80a4c78bbb5d1305788bad977ed4ed4f232fb66
Mhayk Whandson <hi@mhayk.com>
2019-08-08 02:13:37 -0400

    Service api has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-export default function Main() {
+import logo from '../assets/logo.svg'
+import dislike from '../assets/'
+
+export default function Main({ match }) {
     return (
-        <h1>Main...</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/1500873?v=4" alt="" />
+                    <footer>
+                        <strong>Mhayk Whandson</strong>
+                        <p>Programador apaixonado!</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={} />
+                        </button>
+                        <button type="button">
+                            <img src={} />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
